19f0270687ce663a776cc219968150b948782e34
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2023-09-20 07:05:19 -0600

    change: footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <Container>© Benjamin Wiederkehr</Container>
+      <Container>Made with ❤️ by Benjamin Wiederkehr</Container>
     </footer>
   );
 }
