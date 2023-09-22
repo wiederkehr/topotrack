05afa44bb1d991a58252aed9773dd46a95c91259
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2023-09-22 12:10:22 -0600

    add(header): Connect with Strava logo

--- src/components/Header/UserSignedOut.js
+++ src/components/Header/UserSignedOut.js
@@ -1,10 +1,16 @@
+import Image from "next/image";
 import { signIn } from "next-auth/react";
 import styles from "./UserSignedOut.module.css";
 
 export default function UserSignedOut() {
   return (
     <button className={styles.action} onClick={() => signIn("strava")}>
-      Sign in with Strava
+      <Image
+        src="/images/btn_strava_connectwith_orange.svg"
+        height={48}
+        width={193}
+        alt="Connect with Strava"
+      />
     </button>
   );
 }
