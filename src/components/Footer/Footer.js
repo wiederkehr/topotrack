6e82c7bc4f0bacd16e4ed1e65e97afa2482f0294
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2023-09-22 10:17:21 -0600

    change: replace emoji with icon in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,16 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import Container from "@/components/Container";
 import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <Container>Made with ❤️ by Benjamin Wiederkehr</Container>
+      <Container>
+        Made with{" "}
+        <FontAwesomeIcon icon={faHeart} className={styles.footerHeartIcon} /> by
+        Benji
+      </Container>
     </footer>
   );
 }
