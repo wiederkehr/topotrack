8e03631f683a2f45f51f4f1e44ba962472e2f366
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2023-09-22 12:10:12 -0600

    add(footer): Powered by Strava logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import Container from "@/components/Container";
@@ -7,9 +9,28 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <Container>
-        Made with{" "}
-        <FontAwesomeIcon icon={faHeart} className={styles.footerHeartIcon} /> by
-        Benji
+        <div className={styles.footerGrid}>
+          <div className={styles.footerGridLeft}>
+            Made with{" "}
+            <FontAwesomeIcon
+              icon={faHeart}
+              className={styles.footerHeartIcon}
+            />{" "}
+            by{" "}
+            <Link href="https://benjaminwiederkehr.com">
+              {" "}
+              Benjamin Wiederkehr
+            </Link>
+          </div>
+          <div className={styles.footerGridRight}>
+            <Image
+              src="/images/api_logo_pwrdBy_strava_horiz_gray.svg"
+              height={32}
+              width={171.42}
+              alt="Powered by Strava"
+            />
+          </div>
+        </div>
       </Container>
     </footer>
   );
