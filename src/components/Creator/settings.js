c456fabd9f2af27190196e708443149d10e718f8
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2023-09-27 19:40:16 -0600

    add: settings with ratios

---
+++ src/components/Creator/settings.js
@@ -0,0 +1,8 @@
+export const ratios = [
+  { name: "Keynote", width: 1920, height: 1080 },
+  { name: "Landscape", width: 1200, height: 600 },
+  { name: "Portrait", width: 600, height: 1200 },
+  { name: "Square", width: 1200, height: 1200 },
+  { name: "Notion", width: 2400, height: 400 },
+  { name: "Custom" },
+];
