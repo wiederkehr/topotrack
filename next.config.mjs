504c177ef7448632fb99f15755e9310e53881438
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2024-10-06 11:53:39 -0600

    change(global): messing around with eslint and prettier

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/**
+ * @see https://nextjs.org/docs/app/api-reference/next-config-js
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default nextConfig;
