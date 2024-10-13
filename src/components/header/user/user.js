e45aeed127f9df41496cf2fceaa0089d84441b93
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2024-10-12 18:12:38 -0600

    refine(header): user popover

--- src/components/header/user/user.js
+++ src/components/header/user/user.js
@@ -47,26 +47,36 @@ export default function User({ user }) {
             </Link>
           </Text>
         </Box>
-        <Separator my="3" size="4" />
-        <SegmentedControl.Root
-          onValueChange={(value) => setTheme(value)}
-          defaultValue={theme}
-          className={styles.userSegmentedControl}
-        >
-          <SegmentedControl.Item value="light">
-            <Flex align={"center"} gap={"1"}>
-              <SunIcon />
-              Light
-            </Flex>
-          </SegmentedControl.Item>
-          <SegmentedControl.Item value="dark">
-            <Flex align={"center"} gap={"1"}>
-              <MoonIcon />
-              Dark
-            </Flex>
-          </SegmentedControl.Item>
-        </SegmentedControl.Root>
-        <Separator my="3" size="4" />
+        <Separator my="4" size="4" />
+        <Flex direction="column" gap="2">
+          <Heading as="h3" size="3">
+            Settings
+          </Heading>
+          <Flex direction="column" gap="2">
+            <Heading as="h3" size="2">
+              Theme
+            </Heading>
+            <SegmentedControl.Root
+              onValueChange={(value) => setTheme(value)}
+              defaultValue={theme}
+              className={styles.userSegmentedControl}
+            >
+              <SegmentedControl.Item value="light">
+                <Flex align={"center"} gap={"1"}>
+                  <SunIcon />
+                  Light
+                </Flex>
+              </SegmentedControl.Item>
+              <SegmentedControl.Item value="dark">
+                <Flex align={"center"} gap={"1"}>
+                  <MoonIcon />
+                  Dark
+                </Flex>
+              </SegmentedControl.Item>
+            </SegmentedControl.Root>
+          </Flex>
+        </Flex>
+        <Separator my="4" size="4" />
         <form action={signOutAction}>
           <Button type="submit" color="red" className={styles.userAction}>
             Sign out
