e60138c47bbef1eae806e1fdb21416fb31cfae0a
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2025-04-25 17:37:00 -0600

    change: disable all templates except static

--- src/features/templates/index.js
+++ src/features/templates/index.js
@@ -2,6 +2,6 @@ import templateAnimation from "./templateAnimation";
 import templateDebug from "./templateDebug";
 import templateStatic from "./templateStatic";
 
-const templates = [templateStatic, templateAnimation, templateDebug];
+const templates = [templateStatic];
 
 export default templates;
