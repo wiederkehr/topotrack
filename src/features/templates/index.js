e1e95d2224aefd66284b1dfeb40806ab00f45a8e
Benjamin Wiederkehr <bwiederkehr@apple.com>
2025-10-17 09:15:49 -0600

    change: template order

--- src/features/templates/index.js
+++ src/features/templates/index.js
@@ -1,4 +1,4 @@
 import templateAnimation from "./templateAnimation";
 import templateStatic from "./templateStatic";
 
-export const templates = [templateStatic, templateAnimation];
+export const templates = [templateAnimation, templateStatic];
